74de6eb97a2aa18cf317ccbf35950e8be13b5458
CderLord <biglordts@gmail.com>
2025-08-15 23:17:38 -0300

    feat:add section about me

---
+++ src/components/AboutMeSection.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import Text from './ui/VText.vue';
+import AboutImage from '@/assets/about-me.jpg'
+</script>
+
+<template>
+    <div class="section-container">
+        <div class="section-img-container">
+            <img class="section-img-content" :src="AboutImage" alt="about-me"></img>
+        </div>
+        <div class="section-content">
+            <Text style="margin: 0px;"type="highlight">Sobre mim</Text>
+            <Text style="margin: 0px;" type="description">Olá! Eu sou um profissional multidisciplinar que une design e desenvolvimento para criar
+                interfaces limpas, eficientes e memoráveis. Acredito em um processo colaborativo, orientado por dados e
+                que valoriza a simplicidade e a performance.
+
+                Minha especialidade é transformar ideias em produtos digitais com código moderno, acessível e
+                escalável.</Text>
+        </div>
+
+    </div>
+
+</template>
+
+<style>
+</style>
--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts">
-import Image from '@/assets/Image.jpg';
+import Image from '@/assets/hero.jpg';
 import Text from './ui/VText.vue';
 import Button from './ui/VButton.vue'
 </script>
 
 <template>
-    <div class="container-intro">
-        <div class="intro">
+    <div class="section-container">
+        <div class="section-content">
             <Text style="margin: 0px;" type="title">Potfólio Profissional</Text>
             <Text style="margin: 0px;" type="highlight">Criatividade que entrega resultados</Text>
             <Text style="margin: 0px;" type="description">Sou designer e desenvolvedor focado em construir experiências
@@ -19,47 +19,13 @@ import Button from './ui/VButton.vue'
                 </div>
             </div>
         </div>
-        <div class="img-intro">
-            <img :src="Image" alt="Descrição da imagem">
+        <div class="section-img-container">
+            <img class="section-img-content" :src="Image" alt="Descrição da imagem">
         </div>
     </div>
 </template>
 
 <style scoped>
-div.container-intro {
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    gap: 20px;
-    padding: 20px;
-}
-
-.intro {
-    flex: 1;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    gap: 1.5rem;
-}
-
-.img-intro {
-    flex: 1;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    background: white;
-    padding: 20px;
-    box-sizing: border-box;
-    text-align: right;
-}
-
-.img-intro img {
-    margin-top: 8rem;
-    width: 100%;
-    height: 100%;
-    border-radius: 10px;
-}
 
 .buttons {
     flex: 1;
