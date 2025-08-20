c2a2d9eb9b276119cd5217162d2485ed3440d6d2
CderLord <biglordts@gmail.com>
2025-08-20 18:11:23 -0300

    feat: add FooterSection

---
+++ src/components/FooterSection.vue
@@ -0,0 +1,23 @@
+<script setup>
+import VText from './ui/VText.vue';
+
+
+</script>
+
+<template>
+    <div class="container-footer">
+        <VText type="description">
+            &copy; 2025 Andre Tiago — Todos os direitos reservados
+        </VText>
+    </div>
+
+</template>
+
+<style>
+.container-footer {
+    background-color: white;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+}
+</style>
