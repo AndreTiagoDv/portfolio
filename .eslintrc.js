1392cdb739c0a4d2708af0579c76b3ccaf5f46b4
CderLord <biglordts@gmail.com>
2025-08-26 22:08:58 -0300

    build: add config prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,14 @@
 module.exports = {
   extends: [
     // add more generic rulesets here, such as:
-    // 'eslint:recommended',
-    'plugin:vue/recommended',
+    'eslint:recommended',
+    'plugin:vue/vue3-recommended',
+    'prettier',
+
     // 'plugin:vue/vue2-recommended' // Use this if you are using Vue.js 2.x.
   ],
   rules: {
     // override/add rules settings here, such as:
     // 'vue/no-unused-vars': 'error'
-  }
-}
+  },
+};
