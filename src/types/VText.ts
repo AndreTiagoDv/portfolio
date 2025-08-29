9cafb11f6ca68f0bd9313b775f4f1b8f6629201d
CderLord <biglordts@gmail.com>
2025-08-29 14:45:47 -0300

    fix: adjust ESLint config and fix altered components

--- src/types/VText.ts
+++ src/types/VText.ts
@@ -3,5 +3,5 @@ export type TextType = 'highlight' | 'title' | 'paragraph' | 'description';
 export interface VTextProps {
   type?: TextType;
   class?: string;
-  [key:string]: unknown; //permite outros atributos html
+  [key: string]: unknown; //permite outros atributos html
 }
