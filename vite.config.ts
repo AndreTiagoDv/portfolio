8afa02111cb26c82ca4c8081d8316f55f79cbe68
AndreDev <biglordts@gmail.com>
2025-09-10 16:58:12 -0300

    fix: dependeces scss

--- vite.config.ts
+++ vite.config.ts
@@ -12,4 +12,13 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+          @use "@/assets/styles/_breakpoints.scss" as *;
+        `,
+      },
+    },
+  },
 });
