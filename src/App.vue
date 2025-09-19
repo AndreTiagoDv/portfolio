ddb5ef6c612caf2dfd668483552603e0c6483ef8
AndreDev <biglordts@gmail.com>
2025-09-19 10:38:23 -0300

    feat: create pages directory and landingpage folder

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,12 @@
 <script setup>
-  import SectionHeader from './components/sections/SectionHeader.vue';
-  import SectionHero from './components/sections/SectionHero.vue';
-  import SectionAboutMe from './components/sections/SectionAboutMe.vue';
-  import SectionProjects from './components/sections/SectionProjects.vue';
-  import SectionSkills from './components/sections/SectionSkills.vue';
-  import SectionContacts from './components/sections/SectionContacts.vue';
+  import LandingPage from './components/pages/LandingPage.vue';
   import SectionFooter from './components/sections/SectionFooter.vue';
+  import SectionHeader from './components/sections/SectionHeader.vue';
 </script>
 
 <template>
   <SectionHeader />
-  <SectionHero />
-  <SectionAboutMe />
-  <SectionProjects />
-  <SectionSkills />
-  <SectionContacts />
+  <LandingPage />
   <SectionFooter />
 </template>
 
